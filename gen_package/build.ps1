6cca4f460b0cd55d2bc6f09de75608d02220c455
Vladyslav Serhiienko <vlad.serhiienko@gmail.com>
2024-10-06 17:41:35 -0700

    PS1 scripts

---
+++ gen_package/build.ps1
@@ -0,0 +1,11 @@
+
+New-Item -ItemType Directory -Force -Path "build_msvc_amd64"
+Set-Location -Path "build_msvc_amd64"
+
+conan install .. --build=missing --profile:host=../../profiles/msvc_amd64 --profile:build=../../profiles/msvc_amd64 -s build_type=Debug
+conan install .. --build=missing --profile:host=../../profiles/msvc_amd64 --profile:build=../../profiles/msvc_amd64 -s build_type=Release
+cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake"
+cmake --build . --config Release
+./Release/CoquelicotFormatGen.exe
+
+Set-Location -Path ".."
